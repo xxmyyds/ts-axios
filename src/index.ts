b4f2085a7b3c62c9d1e2f7d5de0de0f4e1987043
Code_xxmyyds <xxmyyds@163.com>
2021-08-14 21:31:50 +0800

    feat(all file): 完成url和body基础功能编写

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import { AxiosRequestConfig } from './types'
+import { bulidURL } from './helpers/url'
+import { transformRequest } from './helpers/data'
+import xhr from './xhr'
+
+function axios(config: AxiosRequestConfig): void {
+  processConfig(config)
+  xhr(config)
+}
+
+function processConfig(config: AxiosRequestConfig): void {
+  config.url = transformURL(config)
+  config.data = transformRequestData(config)
+}
+
+function transformURL(config: AxiosRequestConfig): string {
+  const { url, params } = config
+  return bulidURL(url, params)
+}
+
+function transformRequestData(config: AxiosRequestConfig): any {
+  return transformRequest(config.data)
+}
+export default axios
