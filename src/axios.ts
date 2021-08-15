fe049f365abdba5be6c332471c3bf020b3c332f8
Code_xxmyyds <xxmyyds@163.com>
2021-08-15 15:44:57 +0800

    feat(all the files): 完成接口的扩展
    扩展了axios接口，函数的重载以及响应数据支持泛型

--- src/axios.ts
+++ src/axios.ts
@@ -1,45 +1,17 @@
-import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
-import { bulidURL } from './helpers/url'
-import { transformRequest, tranformResponse } from './helpers/data'
-import { processHeaders } from './helpers/headers'
-import xhr from './xhr'
+import Axios from './core/Axios'
+import { AxiosIntance } from './types'
+import { extend } from './helpers/util'
 
-// 入口函数
-function axios(config: AxiosRequestConfig): AxiosPromise {
-  processConfig(config)
-  return xhr(config).then(res => {
-    return transformResponseData(res)
-  })
+// 创建混合对象的工厂函数
+function createInstance(): AxiosIntance {
+  const context = new Axios()
+
+  const instance = Axios.prototype.request.bind(context)
+
+  extend(instance, context)
+
+  return instance as AxiosIntance
 }
 
-// 处理config的函数
-function processConfig(config: AxiosRequestConfig): void {
-  config.url = transformURL(config)
-  config.headers = transformHeaders(config)
-  config.data = transformRequestData(config)
-}
-
-// 运输处理后的url的函数
-function transformURL(config: AxiosRequestConfig): string {
-  const { url, params } = config
-  return bulidURL(url, params)
-}
-
-// 运输处理后的data函数
-function transformRequestData(config: AxiosRequestConfig): any {
-  return transformRequest(config.data)
-}
-
-// 运输处理后的headers的函数
-function transformHeaders(config: AxiosRequestConfig): any {
-  const { headers = {}, data } = config
-  return processHeaders(headers, data)
-}
-
-// 运输处理后的响应数据的函数
-function transformResponseData(res: AxiosResponse): AxiosResponse {
-  res.data = tranformResponse(res.data)
-  return res
-}
-
+const axios = createInstance()
 export default axios
---
+++ src/core/Axios.ts
@@ -0,0 +1,71 @@
+import { AxiosRequestConfig, AxiosPromise, Method } from './../types/index'
+import dispatchRequest from './dispatchRequest'
+export default class Axios {
+  request(url: any, config?: any): AxiosPromise {
+    if (typeof url === 'string') {
+      if (!config) {
+        config = {}
+      }
+      config.url = url
+    } else {
+      config = url
+    }
+    return dispatchRequest(config)
+  }
+
+  get(url: string, config?: AxiosRequestConfig): AxiosPromise {
+    return this._requestMethodWithOutData('get', url, config)
+  }
+
+  delete(url: string, config?: AxiosRequestConfig): AxiosPromise {
+    return this._requestMethodWithOutData('delete', url, config)
+  }
+
+  head(url: string, config?: AxiosRequestConfig): AxiosPromise {
+    return this._requestMethodWithOutData('head', url, config)
+  }
+
+  options(url: string, config?: AxiosRequestConfig): AxiosPromise {
+    return this._requestMethodWithOutData('options', url, config)
+  }
+
+  post(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise {
+    return this._requestMethodWithData('post', url, data, config)
+  }
+
+  put(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise {
+    return this._requestMethodWithData('put', url, data, config)
+  }
+
+  patch(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise {
+    return this._requestMethodWithData('patch', url, data, config)
+  }
+
+  _requestMethodWithOutData(
+    method: Method,
+    url: string,
+    config?: AxiosRequestConfig
+  ): AxiosPromise {
+    return this.request(
+      Object.assign(config || {}, {
+        method,
+        url
+      })
+    )
+  }
+
+  _requestMethodWithData(
+    method: Method,
+    url: string,
+    data?: any,
+    config?: AxiosRequestConfig
+  ): AxiosPromise {
+    return this.request(
+      Object.assign(config || {}, {
+        method,
+        url,
+        data
+      })
+    )
+  }
+}
